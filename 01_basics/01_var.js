b013cd2ba0bc40d1641b9aac92d80e6eecb2eefb
imsarthak1706 <sarthakkumar1706@gmail.com>
2026-02-10 13:18:20 +0000

    variables

---
+++ 01_basics/01_var.js
@@ -0,0 +1,11 @@
+const accountID=12345
+let accountEmail="sarthak@google.com"
+var accountPassword="12345"
+accountCity="Dhanbad"
+let accountState;
+
+
+accountEmail="sarthak@1706"
+accountPassword="7654321"
+accountCity="DHANBAD"
+console.table([accountID,accountEmail,accountPassword,accountCity])
